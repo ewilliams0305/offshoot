c532c5559cf2e2f69c60c7f9002a8aa9c4265418
Eric Williams <53901011+ewilliams0305@users.noreply.github.com>
2023-10-30 20:39:51 -0400

    Update functions.go

--- module/functions.go
+++ module/functions.go
@@ -9,3 +9,11 @@ type EnsureFunction[TInput any] func(input TInput) bool
 
 // A tap function is used as an opertunity to access the value stored inside the [offshoot]
 type TapFunction[TValue any] func(value TValue)
+
+// The error handle function provides the consumer an opportunity 
+// to handle an error and return a new offshoot or either failure or success
+type ErrorHandle[TValue any] func(error Error) Offshoot[TValue]
+
+// The value handle function provides the consuler an opportunity 
+// to handle an value and create a new Offshoot 
+type ValueHandle[TValue any] func(value TValue) Offshoot[TValue]
